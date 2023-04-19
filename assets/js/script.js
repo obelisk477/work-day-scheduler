36130be784354165f2060d4d5c3f9925af63db6b
obelisk477 <cwbatte2@gmail.com>
2023-04-19 18:27:06 -0400

    Organized and commented CSS and JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 
 $(function () {
+
+  // Logic for button clicks on the save icons
   $('.saveBtn').on('click', function() {
     var currentHourPlan = $(this).siblings('textarea').val()
     var currentHour = $(this).parent().attr('id').match(/\d+/)[0]
     let schedule = localStorage.getItem('schedule')
+
+    // Check for (and handle) empty local storage if applicable; else save current textArea value to corresponding hour in local storage
     if (!schedule) {
       let scheduleObj = {}
       scheduleObj[currentHour] = currentHourPlan
@@ -15,9 +19,10 @@ $(function () {
     }
   })
 
-
+  // Grab all hour blocks
   var hourBlocks = $('.time-block')
-  
+
+  // Loop through hour blocks and set classes (and therefore styles) for past, present, and future time blocks
   $.each(hourBlocks, function() {
     let hour = Number($(this).attr("id").match(/\d+/)[0])
     let nowHour = Number(dayjs().format('H'))
@@ -30,6 +35,7 @@ $(function () {
     }
   })
 
+  // Loop through local storage, grab all textareas (hours), and add saved events to relevant hours
   var scheduleObj = JSON.parse(localStorage.getItem('schedule')) ? JSON.parse(localStorage.getItem('schedule')) : {}
   var scheduleObjKeys = Object.keys(scheduleObj)
   let allTextAreas = $('textarea')
@@ -40,6 +46,7 @@ $(function () {
     }
   })
 
+  // Helper function for adding ordinal text to end of day
   let getOrdinalEnding = function() {
     let today = Number(dayjs().format('D'))
     if (today == 1 || today == 21 || today == 31) {
@@ -53,6 +60,7 @@ $(function () {
     }
   }
 
+  // Set date at top of page
   let ordinalEnding = getOrdinalEnding()
   let today = dayjs().format('dddd, MMMM D')
   $('#currentDay').text(today+ordinalEnding)
